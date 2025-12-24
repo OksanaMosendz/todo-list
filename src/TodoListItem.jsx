ce1c13ab3fb3f0292ab3132810fa288a4021308f
Oksana Mosendz <bagheera03@gmail.com>
2025-12-23 20:43:52 -0600

    Add empty state message, disable add button, use controlled form

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,7 +1,8 @@
-function TodoListItem({todo}){
+function TodoListItem({todo, onCompleteTodo}){
    
  return(
-   <li key={todo.id}>{todo.title}</li>
+   <li key={todo.id}><form>
+   <input onChange={()=>onCompleteTodo(todo.id)} type="checkbox" checked={todo.isCompleted}/>{todo.title}</form></li>
 )
 }
 
