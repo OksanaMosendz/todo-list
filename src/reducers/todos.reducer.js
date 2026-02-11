e98a41b85aa72aaa23eae818d8bf0dc0ec2f8a45
Oksana Mosendz <bagheera03@gmail.com>
2026-02-11 11:20:09 -0600

    create reducer

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,3 +1,10 @@
+const initialState = {
+  todoList: [],
+  isLoading: false,
+  errorMessage: '',
+  isSaving: false,
+};
+
 const actions = {
   fetchTodos: 'fetchTodos',
   loadTodos: 'loadTodos',
@@ -14,11 +21,63 @@ const actions = {
   endRequest: 'endRequest',
 };
 
-const initialState = {
-  todoList: [],
-  isLoading: false,
-  errorMessage: '',
-  isSaving: false,
-};
+function reducer(state = initialState, action) {
+   
+  switch (action.type) {
+
+    case actions.fetchTodos:
+      return {
+        ...state,
+      };
+
+    case actions.loadTodos:
+      return {
+        ...state,
+      };
+
+    case actions.revertTodos:
+      return {
+        ...state,
+      };
+
+    case actions.addTodo:
+      return {
+        ...state,
+      };
+
+    case actions.updateTodo:
+      return {
+        ...state,
+      };
+
+    case actions.completeTodo:
+      return {
+        ...state,
+      };
+
+    case actions.setError:
+      return {
+        ...state,
+      };
+
+    case actions.clearError:
+      return {
+        ...state,
+      };
+
+    case actions.startRequest:
+      return {
+        ...state,
+      };
+
+    case actions.endRequest:
+      return {
+        ...state,
+      };
+
+    default:
+      break;
+  }
+}
 
 export { initialState, actions };
