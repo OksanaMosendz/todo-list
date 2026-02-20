298364b9f3556f6130ce6e03c66257c0a6ac1730
Oksana Mosendz <bagheera03@gmail.com>
2026-02-20 14:54:13 -0600

    add header&nav

--- src/pages/TodosPage.jsx
+++ src/pages/TodosPage.jsx
@@ -1,26 +1,26 @@
 import { StateContext } from '../stateContext';
 import { useContext } from 'react';
 import TodoList from '../features/TodoList/TodoList';
-import TodoForm from '../TodoForm'; 
+import TodoForm from '../TodoForm';
 import TodoViewForm from '../features/TodoViewForm';
 
+function TodosPage({ onCompleteTodo, onUpdateTodo, onAddTodo }) {
+  const { todoState } = useContext(StateContext);
 
-function TodosPage ({onCompleteTodo,onUpdateTodo,onAddTodo}) {
+  return (
+    <>
+      <TodoForm onAddTodo={onAddTodo}></TodoForm>
 
-const { todoState } = useContext(StateContext);
-
-return (
-<> 
-  <TodoForm onAddTodo={onAddTodo} ></TodoForm>
-
-   <TodoList
+      <TodoList
         onCompleteTodo={onCompleteTodo}
         onUpdateTodo={onUpdateTodo}
         todoList={todoState.todoList}
         isLoading={todoState.isLoading}
-    ></TodoList>
+      ></TodoList>
 
       <TodoViewForm />
-    </>)}
-    
-    export default TodosPage;
+    </>
+  );
+}
+
+export default TodosPage;
