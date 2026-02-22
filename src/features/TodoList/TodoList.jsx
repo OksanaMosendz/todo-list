91da22f31166c94c08d277c6cc53ec814d409856
Oksana Mosendz <bagheera03@gmail.com>
2026-02-21 18:30:39 -0600

    add pagination calculation

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,17 +1,24 @@
 import styles from './TodoList.module.css'
 import TodoListItem from './TodoListItem';
+import { useSearchParams } from 'react-router';
+
 
 function TodoList({ todoList, onUpdateTodo, isLoading }) {
   const filtredTodoList = todoList.filter((todo) => !todo.isCompleted);
+const [searchParams, setSearchParams] = useSearchParams();
+ const currentPage=parseInt(searchParams.get('page')||'1', 10);
+const itemsPerPage=15;
+const indexOfFirstTodo=(currentPage-1)*itemsPerPage;
+const totalPages=Math.ceil(filtredTodoList.length/itemsPerPage);
 
-  return filtredTodoList.length === 0 ? (
+return filtredTodoList.length === 0 ? (
     isLoading ? (
       <p>Todo list loading...</p>
     ) : (
       <p>Add todo above to get started</p>
     )
   ) : (
-    <ul className={styles.list}>
+    <><ul className={styles.list}>
       {filtredTodoList.map((todo) => (
         <TodoListItem
           onCompleteTodo={onUpdateTodo}
@@ -21,6 +28,8 @@ function TodoList({ todoList, onUpdateTodo, isLoading }) {
         />
       ))}
     </ul>
+
+</>
   );
 }
 
