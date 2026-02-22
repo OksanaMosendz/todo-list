1189bcada5609c026b8f7471ccf4faea2407c361
Oksana Mosendz <bagheera03@gmail.com>
2026-02-22 12:10:54 -0600

    add nav ui and handlers

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,35 +1,61 @@
-import styles from './TodoList.module.css'
+import styles from './TodoList.module.css';
 import TodoListItem from './TodoListItem';
-import { useSearchParams } from 'react-router';
-
+import { useSearchParams } from 'react-router-dom';
 
 function TodoList({ todoList, onUpdateTodo, isLoading }) {
   const filtredTodoList = todoList.filter((todo) => !todo.isCompleted);
-const [searchParams, setSearchParams] = useSearchParams();
- const currentPage=parseInt(searchParams.get('page')||'1', 10);
-const itemsPerPage=15;
-const indexOfFirstTodo=(currentPage-1)*itemsPerPage;
-const totalPages=Math.ceil(filtredTodoList.length/itemsPerPage);
 
-return filtredTodoList.length === 0 ? (
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+  const itemsPerPage = 5;
+  const indexOfFirstTodo = (currentPage - 1) * itemsPerPage;
+  const indexOfLastTodo = indexOfFirstTodo + itemsPerPage;
+  const totalPages = Math.ceil(filtredTodoList.length / itemsPerPage);
+  const currentPageTodos = filtredTodoList.slice(
+    indexOfFirstTodo,
+    indexOfLastTodo
+  );
+
+  function handlePreviousPage() {
+    if (currentPage > 1) {
+      setSearchParams({ page: currentPage - 1 });
+    }
+  }
+
+  function handleNextPage() {
+    if (currentPage < totalPages) {
+      setSearchParams({ page: currentPage + 1 });
+    }
+  }
+
+  return filtredTodoList.length === 0 ? (
     isLoading ? (
       <p>Todo list loading...</p>
     ) : (
       <p>Add todo above to get started</p>
     )
   ) : (
-    <><ul className={styles.list}>
-      {filtredTodoList.map((todo) => (
-        <TodoListItem
-          onCompleteTodo={onUpdateTodo}
-          onUpdateTodo={onUpdateTodo}
-          key={todo.id}
-          todo={todo}
-        />
-      ))}
-    </ul>
+    <>
+      <ul className={styles.list}>
+        {currentPageTodos.map((todo) => (
+          <TodoListItem
+            onCompleteTodo={onUpdateTodo}
+            onUpdateTodo={onUpdateTodo}
+            key={todo.id}
+            todo={todo}
+          />
+        ))}
+      </ul>
 
-</>
+      <div className={styles.paginationControls}>
+        <button onClick={handlePreviousPage}>Previous</button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNextPage}>Next</button>
+      </div>
+    </>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 function About() {
   return (
     <>
+    <p>TodoListApp - your daily task manager</p>
+      <p>Made with React</p>
       <p>Author: Oksana Mosendz</p>
       <p> &copy; 2025</p>
     </>
